b822fdcbfca32685706efa65bcbbb75b64224571
MaksymenkoMaksym <106187792+MaksymenkoMaksym@users.noreply.github.com>
2022-08-13 12:45:32 +0000

    Deploying to Maks-pages from @ MaksymenkoMaksym/my-first-template@84a4e132c59e0d718056b7dddb26732661c5e26d 🚀

---
+++ index.713fbc63.js
@@ -0,0 +1,2 @@
+console.log("Hello world!"),console.log("Hello world!"),console.log("Hello world!"),console.log("Hello world!");
+//# sourceMappingURL=index.713fbc63.js.map
---
+++ index.a653d2ed.js
@@ -0,0 +1,2 @@
+console.log("Hello world!"),console.log("Hello world!"),console.log("Hello world!"),console.log("Hello world!");
+//# sourceMappingURL=index.a653d2ed.js.map
